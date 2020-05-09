d28456cf73b157f5cae945b20b4f413aa36c785a
colinxs <me@colinxsummers.com>
2020-05-08 17:50:47 -0700

    save

---
+++ bench.jl
@@ -0,0 +1,19 @@
+using BenchmarkTools
+
+function foo!(A, B)
+    Threads.@threads for i = eachindex(A, B)
+        A[i] = B[i] * 2 + rand()
+    end
+end
+
+B = rand(100)
+A = similar(B)
+@btime foo!($A, $B)
+
+B = rand(1000)
+A = similar(B)
+@btime foo!($A, $B)
+
+B = rand(10000)
+A = similar(B)
+@btime foo!($A, $B)
